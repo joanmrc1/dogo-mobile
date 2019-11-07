7b3ac5bb4f454f6a90b56cd699f02360b63b2f63
Joan Marcos <joan.santos@soterotech.com.br>
2019-11-07 00:04:53 -0300

    Adicionado Tela oara cadastro de vermifugacao.

---
+++ src/pages/Pets/Vermifugation/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { Item, Input,Label, Row, Picker} from 'native-base';
+import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import NavigationService from '../../../services/navigation';
+
+export default function SelectInfoPet() {
+
+	return (
+		<View> 
+			<Text> asd </Text>
+		</View>
+	);
+}
