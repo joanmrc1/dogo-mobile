1b132cee1cea95870d6b25f5cc8ef25917b3c1fe
Matheus Trindade <trindadematheus27@gmail.com>
2019-11-09 03:17:54 -0300

    Mudando todas as cores

--- src/pages/Profile/ProfileScreen.js
+++ src/pages/Profile/ProfileScreen.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text} from 'react-native';
-
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
 
 export default function Pets() {
-	return (
-		<View>
-			<Text> Meu Perfil </Text>
-		</View>
-	);
+  return (
+    <View>
+      <Text> Meu Perfil </Text>
+    </View>
+  );
 }
